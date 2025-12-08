762de721ffa07504dff07f8e9b83fad71df48e33
liamkuchta <liamkuchta@gmail.com>
2025-12-08 13:47:08 -0500

    html css and added php files
    html css and added php files

---
+++ includes/send.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "liamkuchta@gmail.com";
+    $subject = "New Message from Portfolio Contact Form";
+
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    $headers = "From: $email";
+
+    // uncomment this if you want to send real emails:
+     mail($to, $subject, $body, $headers);
+
+    echo "<h1>Thank you for messaging me, $name!</h1>";
+    echo "<p>Your message has been sent successfully.</p>";
+
+    exit;
+}
+?>
